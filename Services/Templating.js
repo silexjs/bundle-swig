9e66a611c14c6e29dac5e4264ed3e162dee4e605
Valentin <sitexw@gmail.com>
2015-01-23 18:32:18 +0100

    Big update

--- Services/Templating.js
+++ Services/Templating.js
@@ -8,23 +8,21 @@ var Templating = function(kernel) {
 	this.swig.setDefaults({
 		loader: {
 			resolve: function(to, from) {
-				var toArray = to.split(':');
-				if(toArray.length === 3) {
-					if(toArray[0] !== '') {
-						var bundle = self.kernel.getBundle(toArray[0]);
+				var toView = to.match(/^(.*)\:(.*)\:(.+)$/i);
+				if(toView !== null) {
+					if(toView[1] !== '') {
+						var bundle = self.kernel.getBundle(toView[1]);
 						if(bundle === null) {
-							throw new Error('SILEX.SWIG: The bundle "'+toArray[0]+'" of the view requested does not exist. ('+toArray.join(':')+')');
+							throw new Error('SILEX.SWIG: The bundle "'+toView[1]+'" of the view requested does not exist. ('+to+')');
 						}
 					} else {
 						var bundle = { dir: self.kernel.rootDir+'/app' };
 					}
-					var file = bundle.dir+'/Resources/views/'+(toArray[1]===''?'':toArray[1]+'/')+toArray[2];
+					var file = bundle.dir+'/Resources/views/'+(toView[2]===''?'':toView[2]+'/')+toView[3];
 					if(fs.existsSync(file) === false) {
 						throw new Error('SILEX.SWIG: The view "'+toArray.join(':')+'"  does not exist. ('+file+')');
 					}
 					to = file;
-				} else if(toArray.length !== 0) {
-					throw new Error('SILEX.SWIG: The file path does not comply with the following format: "BUNDLE:DIR:VIEW" or "BUNDLE::VIEW". ('+toSilex.join(':')+')');
 				}
 				if(from !== undefined) {
 					return pa.resolve(pa.dirname(from), to);
@@ -47,15 +45,19 @@ Templating.prototype = {
 	swig: null,
 	
 	renderView: function(view, parameters) {
-		var parameters = parameters || [];
+		var parameters = parameters || {};
 		return this.swig.renderFile(view, parameters);
 	},
 	renderViewResponse: function(view, parameters, request, response) {
 		if(response.getHeader('content-type') === undefined) {
 			response.setHeader('content-type', 'text/html');
 		}
-		response.content += this.swig.renderFile(view, parameters);
+		response.content += this.renderView(view, parameters);
 		response.hasResponse = true;
+	},
+	
+	setFilter: function(filterName, callback) {
+		return this.swig.setFilter(filterName, callback);
 	},
 };
 
